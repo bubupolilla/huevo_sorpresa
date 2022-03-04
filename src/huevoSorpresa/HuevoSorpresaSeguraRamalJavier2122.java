9e8a04a3938e876901e58958ba4f3c2d4b83d241
bubup <bubupolilla_85@hotmail.com>
2022-03-04 18:52:45 +0100

    Mi primer commit huevo_sopresa2122

---
+++ src/huevoSorpresa/HuevoSorpresaSeguraRamalJavier2122.java
@@ -0,0 +1,96 @@
+package huevoSorpresa;
+
+public class HuevoSorpresaSeguraRamalJavier2122 {
+
+    /**
+     * @return the precio
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    private int unidades;//número de huevos que quedan 
+    private double precio;//precio actual de un huevo sorpresa
+    private String sorpresa;//nombre de la sorpresa que incluye huevo sorpresa
+    double precio_maxSeguraRamalFcoJavier2122;//precio máximo que puede tener un huevo sorpresa
+
+    /*Constructor por defecto*/
+    public HuevoSorpresaSeguraRamalJavier2122() {
+    }
+
+
+    /*Constructor por parámetros*/
+    public HuevoSorpresaSeguraRamalJavier2122(int unidades, double precio, String sorpresa, double precio_max) {
+        this.unidades = unidades;
+        this.precio = precio;
+        this.sorpresa = sorpresa;
+        this.precio_maxSeguraRamalFcoJavier2122 = precio_max;
+    }
+
+
+    /*Método que devuelve el número de huevos sorpresa que tiene la tienda*/
+    public int obtenerUnidades() {
+        return this.unidades;
+    }
+
+    /*Método que devuelve el precio que tiene cada huevo sorpresa*/
+    public double obtenerPrecio() {
+        return this.getPrecio();
+    }
+
+
+    /* Método que devuelve la sorpresa que tiene el huevo*/
+    public String obtenerSorpresa() {
+        return sorpresa;
+    }
+
+    /* Método que permite modificar la sorpresa del huevo*/
+    public void modificarSorpresa(String sorpresa) {
+        this.sorpresa = sorpresa;
+    }
+
+
+    /*Método que permite modificar el número de huevos sorpresa que tiene la tienda*/
+    public void modificarUnidades(int unidades) {
+        this.unidades = unidades;
+    }
+
+
+    /*Método que permite sacar huevos sorpresa si se tiene suficiente dinero y hay 
+suficientes unidades en la tienda*/
+    public void sacarHuevosSorpresas(int unidades, double dinero, String sorpresaSeguraRamalFcoJavier2122) throws Exception {
+        if (dinero <= 0) {
+            throw new Exception("Se necesita una cantidad de dinero positiva");
+        }
+        if (dinero < (unidades * getPrecio())) {
+            throw new Exception("No tiene suficiente dinero");
+        }
+        if (unidades <= 0) {
+            throw new Exception("Es necesario indicar una cantidad positiva de huevos sorpresa");
+        }
+        if (this.unidades <= unidades) {
+            throw new Exception("No hay suficientes huevos sorpresa en la tienda");
+        }
+        this.modificarUnidades(this.obtenerUnidades() - unidades);
+    }
+
+
+    /*Método que permite aumentar el precio de venta de un huevo sorpresa, suma al precio actual 
+    el aumento que se indica siempre que no se sobrepase el precio máximo de venta*/
+    public void aumentarPrecio(double aumento) throws Exception {
+        if (aumento <= 0) {
+            throw new Exception("El aumento debe ser positivo");
+        }
+        if (precio_maxSeguraRamalFcoJavier2122 < getPrecio() + aumento) {
+            throw new Exception("No se puede superar el precio máximo");
+        }
+        setPrecio(getPrecio() + aumento);
+    }
+}
